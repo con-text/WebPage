853665f459ec136467a1899a504b8350380b1edf
Maciej Kumorek <mkumorek@gmail.com>
2015-05-01 11:30:41 +0100

    Added Flexbox instead of absolute positioning

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ $(function() {
 	var controller = new ScrollMagic.Controller({triggerHook: 'onLeave'});
 
 	// build scene
-
+	var scene = new ScrollMagic.Scene({
+			triggerElement: "#this-nimble", 
+			offset: 480})
+		.setTween("#icons-layer-1", {opacity: 1}) // trigger a TweenMax.to tween
+		.addTo(controller);
+			
 	/* Band animations */
-	var scene = new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 600})
+	scene = new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 600})
 			.setTween("#meetyourband", {opacity: 1.0}) // trigger a TweenMax.to tween
 			.addTo(controller);
 
 	new ScrollMagic.Scene({triggerElement: "#meet-band", offset: 280})
 			.setPin("#meet-band") 
-			.addIndicators()
 			.addTo(controller);
 
 	/* OS Animations */
@@ -22,7 +26,6 @@ $(function() {
 
 	new ScrollMagic.Scene({triggerElement: "#meet-os", offset: 280})
 			.setPin("#meet-os") 
-			.addIndicators()
 			.addTo(controller);		
 
 	new ScrollMagic.Scene({triggerElement: "#meet-team", duration: 400})
