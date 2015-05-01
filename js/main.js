45af55133e671c1664b9b962b9f9be947879e4a5
Maciej Kumorek <mkumorek@gmail.com>
2015-05-01 13:12:51 +0100

    Added flip

--- js/main.js
+++ js/main.js
@@ -18,14 +18,31 @@ $(function() {
 			.setPin("#meet-band") 
 			.addTo(controller);
 
+	// Move the board from the watch to the center and scale
 	new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 1000, offset: 400})
 			.setTween("#pcb", {left: 670}) // trigger a TweenMax.to tween
 			.addTo(controller);
-
+			
 	new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 1000, offset: 450})
 			.setTween("#pcb", {scale: 3.5}) // trigger a TweenMax.to tween
 			.addTo(controller);
-
+	
+	// Move the back face too		
+	new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 1000, offset: 400})
+		.setTween("#pcb-bottom", {left: 670}) // trigger a TweenMax.to tween
+		.addTo(controller);
+	
+	new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 1000, offset: 1200})
+		.setTween('#pcb', {transform: 'translateX( -100% ) rotateY( -180deg ) scale(3.5)'})
+		.addTo(controller);
+	
+	new ScrollMagic.Scene({triggerElement: "#meet-band", duration: 1000, offset: 1200})
+		.setTween('#pcb-bottom', {transform: 'translateX( -100% ) scale(3.5)'})
+		.addTo(controller);
+	
+	// Now flip both
+	
+	
 	/* OS Animations */
 
 	new ScrollMagic.Scene({triggerElement: "#meet-os", duration: 600})
